e17f28a55a042d30fed8ebe1752d6d7690d6ccd9
Nexuslolz <a.klyosov@inbox.ru>
2023-02-05 13:55:32 +0500

    feat: change navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,7 +4,7 @@ import styles from './Navigation.module.scss';
 const Navigation: React.FC = () => {
   return (
     <nav className={ styles.navigation }>
-      <Link to="/" className={ styles.navigation__item }>Recipes</Link>
+      <Link to="/recipes" className={ styles.navigation__item }>Recipes</Link>
       <Link to="/constructor" className={ styles.navigation__item }>Constructor</Link>
       <Link to="/articles" className={ styles.navigation__item }>Articles</Link>
       <Link to="/about" className={ styles.navigation__item }>About Us</Link>
