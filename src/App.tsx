e7b567408b88a974912548019cb3c3d1ba039210
Nexuslolz <a.klyosov@inbox.ru>
2023-02-07 01:45:28 +0500

    feat: update props for render

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Recipes from './pages/Recipes/Recipes';
@@ -7,8 +7,11 @@ import Articles from './pages/Articles/Articles';
 import AboutUs from './pages/AboutUs/AboutUs';
 import PageNotFound from './pages/404/404';
 import Main from './pages/Main/Main';
+import { URLS } from './types/types';
+
 
 function App() {
+
   return (
     <>
       <Header />
@@ -16,12 +19,11 @@ function App() {
       <Routes>
         <Route path='*' element={ <PageNotFound /> } />
         <Route path='/' element={ <Main /> } />
-        <Route path='/recipes' element={ <Recipes /> } />
+        <Route path='/recipes' element={ <Recipes url={URLS.start} /> } />
         <Route path='/constructor' element={ <Constructor /> } />
         <Route path='/articles' element={ <Articles /> } />
         <Route path='/about' element={ <AboutUs /> } />
       </Routes>
-
       <Footer />
     </>
   );
