f329ca549a2ff825207d1c59b898371d6898839b
Nexuslolz <a.klyosov@inbox.ru>
2023-02-09 23:51:03 +0500

    refactor: decompose react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Recipes from './pages/Recipes/Recipes';
-import Constructor from './pages/Constructor/Constructor';
-import Articles from './pages/Articles/Articles';
-import AboutUs from './pages/AboutUs/AboutUs';
-import PageNotFound from './pages/404/404';
-import Main from './pages/Main/Main';
-import { URLS } from './types/types';
+import AppRouter from './utils/AppRouter';
 
 
 function App() {
@@ -15,15 +8,7 @@ function App() {
   return (
     <>
       <Header />
-
-      <Routes>
-        <Route path='*' element={ <PageNotFound /> } />
-        <Route path='/' element={ <Main /> } />
-        <Route path='/recipes' element={ <Recipes url={URLS.start} /> } />
-        <Route path='/constructor' element={ <Constructor /> } />
-        <Route path='/articles' element={ <Articles /> } />
-        <Route path='/about' element={ <AboutUs /> } />
-      </Routes>
+      <AppRouter/>
       <Footer />
     </>
   );
