507159094676a2889404f0d9d2602998369d2a0f
Nexuslolz <a.klyosov@inbox.ru>
2023-02-10 01:48:14 +0500

    feat: add structure and type for filter item list

--- src/types/types.ts
+++ src/types/types.ts
@@ -64,3 +64,17 @@ export interface IRecipeInfo {
 
     }
 }
+
+export interface IFilters {
+    id: number;
+    query: string;
+    header: string;
+    items: {
+        item1: string;
+        item2: string;
+        item3: string;
+        item4: string;
+        item5: string;
+        item6?: string;
+    }
+}
