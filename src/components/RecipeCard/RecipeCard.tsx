a0caa8627500dc24caa254260388d21adc91c58f
Nexuslolz <a.klyosov@inbox.ru>
2023-02-09 23:55:26 +0500

    feat: update recipe card

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+import FavouriteBtn from '../FavouriteBtn/FavouriteBtn';
 import styles from './RecipeCard.module.scss'
 
 interface IRecipeCard {
-    id: string;
+    id?: string;
     header: string;
     title: string;
     image: string;
@@ -12,10 +14,15 @@ interface IRecipeCard {
 }
 
 const RecipeCard: React.FC<IRecipeCard> = ({id, header, title, image, type = 'happy meal', typeIcon, kcalIcon, kcal}: IRecipeCard) => {
+
+    const router = useNavigate()
   
     return (
-        <div id={id} className={styles.card}>
-            <h3 className={styles.card__header}>{header}</h3>
+        <div onClick={() => router('/recipes/:id')} id={id} className={styles.card}>
+            <div className={styles.card__headerPanel}>
+                <h3 className={styles.card__header}>{header}</h3>
+                <FavouriteBtn/>
+            </div>
                 <img className={styles.card__img} src={image} alt='recipe image'/>
                 <div className={styles.cardParams__wrapper}>
                     <div className={styles.cardParams__item}>
