1a88dde281f079c536ba3a8867118301fbcca474
Nexuslolz <a.klyosov@inbox.ru>
2023-02-09 23:56:44 +0500

    feat: add constant links for request

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const URLS = {
+    start: 'https://api.edamam.com/api/recipes/v2?type=public&app_id=7e3bd218&app_key=c984c0db6f24715dbed35ba4812c946f&imageSize=REGULAR',
+    recipe: 'https://api.edamam.com/api/recipes/v2/',
+}
