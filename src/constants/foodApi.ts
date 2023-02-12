cf12a617b597a4c124d32f13b7e71029d62ca2f6
Nexuslolz <a.klyosov@inbox.ru>
2023-02-12 05:10:29 +0500

    feat: add some keys for request

--- src/constants/foodApi.ts
+++ src/constants/foodApi.ts
@@ -5,3 +5,6 @@ export const NUTRIENTS = 'nutrients';
 
 export const ID = 'b9b5075e';
 export const API_KEY = 'bfb38903c2fd0251dcf47f98780cc404';
+
+export const ID_RECIPE = '7e3bd218'
+export const API_KEY_RECIPE = 'c984c0db6f24715dbed35ba4812c946f'
