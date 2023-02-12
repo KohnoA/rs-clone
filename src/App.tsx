3b5f77817032d9f38b958da2de1a3924c8c0b81f
Nexuslolz <a.klyosov@inbox.ru>
2023-02-12 05:09:24 +0500

    feat: add useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AppRouter from './utils/AppRouter';
+import { FavoriteArrContext } from './components/Favourite/FavoriteBtn/FavoriteBtn';
 
 function App() {
 
   return (
+    <FavoriteArrContext.Provider
+        value={[]}>
     <>
       <Header />
       <AppRouter/>
       <Footer />
     </>
+    </FavoriteArrContext.Provider>
   );
 }
 
