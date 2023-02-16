b9a7c1163935401bd65e52c5f23d5cc858a3b2cf
Nexuslolz <a.klyosov@inbox.ru>
2023-02-16 19:34:04 +0500

    feat: update one recipe page

--- src/components/RecipeInfo/RecipeInfo.tsx
+++ src/components/RecipeInfo/RecipeInfo.tsx
@@ -1,6 +1,6 @@
 import { IRecipeInfo } from '../../types/types';
 import styles from './RecipeInfo.module.scss'
-import RecipeInfoItem from './RecipeInfoItem/RecipeInfoItem';
+// import RecipeInfoItem from './RecipeInfoItem/RecipeInfoItem';
 
 
 const RecipeInfo: React.FC<IRecipeInfo> = ({...props}: IRecipeInfo) => {
@@ -11,28 +11,36 @@ const RecipeInfo: React.FC<IRecipeInfo> = ({...props}: IRecipeInfo) => {
                 <h1 className={styles.recipeInfo__header}>{props.label}</h1>
                 <img className={styles.recipeInfo__img} src={props.image} alt="recipe" />
             </div>
+            <div className={styles.typesWrapper}> 
+                 <div>
+                    <h3>Diet labels</h3>
+                    <ul>
+                        {props.dietLabels?.map((diet, i) => <li key={i}>{diet}</li>)}
+                    </ul>
+                </div>
+                <div>
+                    <h3>Meal type</h3>
+                    <ul>
+                        {props.mealType?.map((type, i) => <li key={i}>{type}</li>)}
+                    </ul>
+                </div>
+                <div>
+                    <h3>Cuisine</h3>
+                    <ul>
+                        {props.cuisineType?.map((cuisine, i) => <li key={i}>{cuisine}</li>)}
+                    </ul>
+                </div>
+            </div>
             <div className={styles.ingredients}>
-            <h3>You need:</h3>
+            <h3 className={styles.ingredients__header}>You need:</h3>
             <ul>
                 {props.ingredientLines?.map((line, i) => <li key={i}>{line}</li>)}
             </ul>
         </div>
         </div>
         <div className={styles.recipeInfoPart}>
-        <div className={styles.nutrients}>
-            <h3>Nutrients</h3>
-            <ul>
-            <li>Calories {(props.calories)?.toFixed(2)+' kcal'}</li>
-                {props.digest?.map((elem, i) => 
-                <li key={i}>
-                    <span>{elem.label} </span>
-                    <span>{(elem.total).toFixed(2)} </span>
-                    <span>{elem.unit} </span>
-                </li>
-                )}
-            </ul>
-        </div>
-            <div className={styles.characteristics}>
+       
+        
                 {/* <RecipeInfoItem
                     header='Health labels'
                     response={props.healthLabels}
@@ -54,27 +62,20 @@ const RecipeInfo: React.FC<IRecipeInfo> = ({...props}: IRecipeInfo) => {
                     <ul>
                         {props.healthLabels?.map((health, i) => <li key={i}>{health}</li>)}
                     </ul>
-                </div> 
-                 <div>
-                    <h3>Diet labels</h3>
-                    <ul>
-                        {props.dietLabels?.map((diet, i) => <li key={i}>{diet}</li>)}
-                    </ul>
                 </div>
-                <div>
-                    <h3>Meal type</h3>
-                    <ul>
-                        {props.mealType?.map((type, i) => <li key={i}>{type}</li>)}
-                    </ul>
-                </div>
-                <div>
-                    <h3>Cuisine</h3>
-                    <ul>
-                        {props.cuisineType?.map((cuisine, i) => <li key={i}>{cuisine}</li>)}
-                    </ul>
-                </div>
-            </div>
-       
+            <div className={styles.nutrients}>
+                <h3>Nutrients</h3>
+                <ul>
+                <li>Calories {(props.calories)?.toFixed(2)+' kcal'}</li>
+                    {props.digest?.map((elem, i) => 
+                    <li key={i}>
+                        <span>{elem.label} </span>
+                        <span>{(elem.total).toFixed(2)} </span>
+                        <span>{elem.unit} </span>
+                    </li>
+                )}
+            </ul>
+        </div>
         </div>
         </div>
     );
