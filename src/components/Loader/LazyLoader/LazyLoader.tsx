a1dc297e1eb35382112f01c6f6573e87be1a52e4
Nexuslolz <a.klyosov@inbox.ru>
2023-02-16 18:49:23 +0500

    feat: create new loader for recipe images

---
+++ src/components/Loader/LazyLoader/LazyLoader.tsx
@@ -0,0 +1,30 @@
+import { useState, useRef, useEffect } from 'react';
+import LoaderFav from '../LoaderFav/LoaderFav';
+import styles from './LazyLoader.module.scss';
+
+export interface ILazyLoader {
+    src: string;
+    alt: string;
+    onLoad?(): void;
+}
+
+const LazyLoader: React.FC<ILazyLoader> = (props: ILazyLoader) => {
+    const {src, alt=''} = props;
+    const [isLoading, setIsLoading] = useState(true)
+    const imgRef = useRef<HTMLImageElement | null>(null)
+
+    useEffect(() => {
+      if(imgRef.current) {
+        imgRef.current.onload = () => setIsLoading(false)
+      }
+    }, [])
+    
+    return (
+        <div className={styles.loader__wrapper}>
+            {isLoading && <LoaderFav/>}
+           <img ref={imgRef} src={src} alt={alt}/> 
+        </div>
+    );
+};
+
+export default LazyLoader;
