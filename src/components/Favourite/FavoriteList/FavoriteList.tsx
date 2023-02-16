9b25d2661294e2c77c497486a98a46778bc039f0
Nexuslolz <a.klyosov@inbox.ru>
2023-02-16 18:26:29 +0500

    feat: update fav list

--- src/components/Favourite/FavoriteList/FavoriteList.tsx
+++ src/components/Favourite/FavoriteList/FavoriteList.tsx
@@ -2,7 +2,7 @@ import { useFetching } from '../../../hooks/useFetching';
 import { IRecipeFavorite, IRecipes } from '../../../types/types';
 import RecipeService from '../../API/RecipeService';
 import RecipeCard from '../../RecipeCard/RecipeCard';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import iconType from '../../../assets/icons/food.svg'
 import kcalIcon from '../../../assets/icons/kcal.svg'
 import LoaderFav from '../../Loader/LoaderFav/LoaderFav';
@@ -17,27 +17,31 @@ const FavoriteList: React.FC<IRecipes> = ({url}: IRecipes) => {
         setRecipes(response.recipe)
     })
 
-    const extractUri = (): string | undefined => {
+    
+    useEffect(() => {
+        fetchingRecipes()
+    }, [])
+    
+    const id = useMemo(() => {
         const mainLink = recipes.uri
         if (!mainLink) return
         const uri = mainLink.slice(mainLink.indexOf('_') + 1)
         return(uri)
-}
+    }, [recipes.uri])
 
-    useEffect(() => {
-      fetchingRecipes()
-    }, [])
-    
+    if(!id) return null
+
     return (
         <div className={styles.favoritePage__wrapper}>
         {errorRecipes && 
-            <div style={{margin:'2em'}}><h1>Error has occured. {errorRecipes}</h1></div>}
+            <div className={styles.wrapperCard}><h1>Error has occured. {errorRecipes}</h1></div>}
             {isRecipesLoading &&
                    <LoaderFav/>}
+                   {}
                           <RecipeCard  
                           route='favorite'
-                          key={1}
-                          id={extractUri()}
+                          key={1}                          
+                          id={id}
                           header={recipes.cuisineType}
                           image={recipes.image}
                           type={recipes.dishType}
