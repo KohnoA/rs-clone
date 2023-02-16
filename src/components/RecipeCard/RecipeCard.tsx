0b7ccfd8744640733efc67f54461b26c6684a225
Nexuslolz <a.klyosov@inbox.ru>
2023-02-16 19:32:54 +0500

    feat: add lazy loader and update styles

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { IRecipeCard } from '../../types/types';
 import FavouriteBtn from '../Favourite/FavoriteBtn/FavoriteBtn';
+import LazyLoader from '../Loader/LazyLoader/LazyLoader';
 import styles from './RecipeCard.module.scss'
 
 const RecipeCard: React.FC<IRecipeCard> = ({route, id, header, title, image, type = 'happy meal', typeIcon, kcalIcon, kcal}: IRecipeCard) => {
@@ -11,9 +12,10 @@ const RecipeCard: React.FC<IRecipeCard> = ({route, id, header, title, image, typ
         <div onClick={() => router(`/${route}/${id}`)} id={id} className={styles.card}>
             <div className={styles.card__headerPanel}>
                 <h3 className={styles.card__header}>{header}</h3>
-                <FavouriteBtn/>
+                <FavouriteBtn cardId={id}/>
             </div>
-                <img className={styles.card__img} src={image} alt='recipe image'/>
+            <LazyLoader src={image} alt='recipe image' />
+                {/* <img className={styles.card__img} src={image} alt='recipe image'/> */}
                 <div className={styles.cardParams__wrapper}>
                     <div className={styles.cardParams__item}>
                     <img className={styles.cardParams__img} src={typeIcon} />
