2126efe931019d3733e6a04e9295b6a69cb31da4
Nexuslolz <a.klyosov@inbox.ru>
2023-02-16 19:45:07 +0500

    feat: create slices

---
+++ src/store/slices/searchSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface ISearchState {
+    searchList: string;
+}
+
+const initialState = (): ISearchState => {
+    const searchList = '';
+    return {searchList}
+}
+
+export const searchSlice = createSlice({
+    name: 'searchSlice',
+    initialState: initialState,
+    reducers: {
+        searchRecipe(state, action: PayloadAction<string>) {
+            let list = state.searchList;
+            const request = action.payload
+
+            list = `${request}`
+            console.log(list)
+        }
+    }
+})
