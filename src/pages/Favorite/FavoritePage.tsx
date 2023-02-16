199c4d97544508a0b51d6c46ddcadc30242b1b63
Nexuslolz <a.klyosov@inbox.ru>
2023-02-16 19:41:36 +0500

    feat: create fav page

--- src/pages/Favorite/FavoritePage.tsx
+++ src/pages/Favorite/FavoritePage.tsx
@@ -1,20 +1,18 @@
-import {useContext, useState} from 'react';
-import RecipeService from '../../components/API/RecipeService';
-import { FavoriteArrContext } from '../../components/Favourite/FavoriteBtn/FavoriteBtn';
+import { useSelector } from 'react-redux';
 import FavoriteList from '../../components/Favourite/FavoriteList/FavoriteList';
 import { URLS } from '../../constants';
 import { API_KEY_RECIPES, ID_RECIPES } from '../../constants/foodApi';
+import { getFavoriteList } from '../../store/selectors/favoriteSelectors';
 import styles from './FavoritePage.module.scss';
 
 const FavoritePage = () => {
-    const favArr = useContext(FavoriteArrContext)
-
+    const favArr = useSelector(getFavoriteList)
     return (
         <div className={styles.favoritePage}>
             <h1>Favorite recipes</h1>
         <div className={styles.favoritePage__wrapper}>
             {favArr.length > 0
-            ? favArr.map((id: string, i: number) =>  <FavoriteList key={i} url={`${URLS.recipe}${id}?type=public&app_id=${ID_RECIPES}&app_key=${API_KEY_RECIPES}`}/> )
+            ? favArr.map((id: string) =>  <FavoriteList key={id} url={`${URLS.recipe}${id}?type=public&app_id=${ID_RECIPES}&app_key=${API_KEY_RECIPES}`}/> )
             : <h1>There is no favorite recipes yet.</h1>}
         </div>
         </div>
