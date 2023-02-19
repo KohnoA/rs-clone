4a3c0211cd126b039c1b58deeeb4bd63194cb760
Nexuslolz <a.klyosov@inbox.ru>
2023-02-19 14:23:20 +0500

    feat: add link to favorite page

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch } from '../../hooks/reduxHooks';
 import { removeUser } from '../../store/slices/userSlice';
 import { useAuth } from '../../hooks/useAuth';
 import { getAuth, signOut } from 'firebase/auth';
+import { Link } from 'react-router-dom';
 
 const Profile: React.FC = () => {
   const [select, setSelect] = useState<boolean>(false);
@@ -24,8 +25,6 @@ const Profile: React.FC = () => {
 
   const myRecipesHandler = () => console.log('Go to my recipes');
 
-  const favoritesHandler = () => console.log('Go to my favorites');
-
   return (
     <div 
       className={ select ? `${styles.profile} ${styles.profile__active}` : styles.profile }
@@ -38,12 +37,15 @@ const Profile: React.FC = () => {
       { select && 
         <div className={ styles.profile__select }>
           <div className={ styles.profile__item } onClick={ myRecipesHandler }>My recipes</div>
-          <div className={ styles.profile__item } onClick={ favoritesHandler }>Favorites</div>
+          <div className={ styles.profile__item }> 
+             <Link to="/favorite" className={ styles.profile__link }>Favorite</Link>
+          </div>
           <div className={ styles.profile__item } onClick={ logOutHandler }>Log out</div>
         </div> 
       }
     </div>
   );
 }
+
 
 export default Profile;
