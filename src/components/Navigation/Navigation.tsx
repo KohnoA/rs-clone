39b695be4103ad612a7c3674d28a62b38a6008f7
Nexuslolz <a.klyosov@inbox.ru>
2023-02-19 14:22:27 +0500

    feat: delete main page

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,11 +4,9 @@ import styles from './Navigation.module.scss';
 const Navigation: React.FC = () => {
   return (
     <nav className={ styles.navigation }>
-      <Link to="/recipes" className={ styles.navigation__item }>Recipes</Link>
       <Link to="/constructor" className={ styles.navigation__item }>Constructor</Link>
       <Link to="/articles" className={ styles.navigation__item }>Articles</Link>
       <Link to="/about" className={ styles.navigation__item }>About Us</Link>
-      <Link to="/favorite" className={ styles.navigation__item }>Favorite</Link>
     </nav>
   );
 }
