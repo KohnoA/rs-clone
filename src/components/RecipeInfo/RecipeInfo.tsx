0e8c30b5d56791b76d622266a167eb8bde7cdb1b
Nexuslolz <a.klyosov@inbox.ru>
2023-02-21 02:47:30 +0500

    feat: update class names and render of items on recipe page

--- src/components/RecipeInfo/RecipeInfo.tsx
+++ src/components/RecipeInfo/RecipeInfo.tsx
@@ -3,10 +3,9 @@ import { IRecipeInfo } from '../../types/types';
 import FavouriteBtn from '../Favourite/FavoriteBtn/FavoriteBtn';
 import LazyLoader from '../Loader/LazyLoader/LazyLoader';
 import styles from './RecipeInfo.module.scss'
-// import RecipeInfoItem from './RecipeInfoItem/RecipeInfoItem';
-
 
 const RecipeInfo: React.FC<IRecipeInfo> = ({...props}: IRecipeInfo) => {
+
     return (
         <div className={styles.recipeInfo}>
             <div className={styles.recipeInfoPart}>
@@ -15,7 +14,19 @@ const RecipeInfo: React.FC<IRecipeInfo> = ({...props}: IRecipeInfo) => {
                  <h1 className={styles.recipeInfo__header}>{props.label}</h1>
                 <FavouriteBtn cardId={props.id!}/>
                </div>
-            <LazyLoader className={styles.recipeImg} src={props.image} alt='recipe image' />
+            <div>
+                <LazyLoader className={styles.recipeImg} src={props.image} alt='recipe image' />
+                <ul  className={`${styles.typesWrapper} ${styles.typesWrapper__main}`}>
+                    {props.digest?.map((digest, i) =>{
+                        if(i < 3) {
+                     return <li key={i}>
+                        <span className={styles.nutrient__header}>{digest.label} </span>
+                        <span className={styles.nutrient__total}>{digest.total.toFixed(2)} </span>
+                        <span>{digest.unit}</span>
+                    </li>
+                    }})}
+                </ul>
+            </div>
             </div>
             <div className={styles.infoWrapper}>
             <div style={{width: '100%'}} className={styles.ingredients}>
@@ -25,22 +36,24 @@ const RecipeInfo: React.FC<IRecipeInfo> = ({...props}: IRecipeInfo) => {
             </ul>
         </div>
             <div className={styles.typesWrapper}>
-                 <div>
-                    <h3>Diet labels</h3>
+            <div>
+                <h3>Cuisine</h3>
                     <ul>
-                        {props.dietLabels?.map((diet, i) => <li key={i}>{diet}</li>)}
+                        {props.cuisineType?.map((cuisine, i) => <li className={styles.mainInfo__item} key={i}>{cuisine}</li>)}
                     </ul>
                 </div>
                 <div>
                     <h3>Meal type</h3>
                     <ul>
-                        {props.mealType?.map((type, i) => <li key={i}>{type}</li>)}
+                        {props.mealType?.map((type, i) => <li className={styles.mainInfo__item} key={i}>{type}</li>)}
                     </ul>
                 </div>
-                <div>
-                    <h3>Cuisine</h3>
+                 <div>
+                    <h3>Diet labels</h3>
                     <ul>
-                        {props.cuisineType?.map((cuisine, i) => <li key={i}>{cuisine}</li>)}
+                        {props.dietLabels?.length !== 0
+                            ? props.dietLabels?.map((diet, i) => <li className={styles.mainInfo__item} key={i}>{diet}</li>)
+                            : <li className={styles.mainInfo__item}>{'No diets there'}</li>}
                     </ul>
                 </div>
             </div>
@@ -65,24 +78,32 @@ const RecipeInfo: React.FC<IRecipeInfo> = ({...props}: IRecipeInfo) => {
                     header='Cuisine type'
                     response={props.cuisineType}
                     /> */}
+                    <div className={styles.nutrient__wrapper}>
+                        <h1>Other:</h1>
+                        <div className={styles.nutrient__info}>
                 <div className={styles.labels}>
                     <h3>Health labels</h3>
-                    <ul>
-                        {props.healthLabels?.map((health, i) => <li key={i}>{health}</li>)}
+                    <ul className={styles.healthWrapper}>
+                        {props.healthLabels?.map((health, i) => <li className={styles.health__item} key={i}>{health}</li>)}
                     </ul>
                 </div>
             <div className={styles.labels}>
                 <h3>Nutrients</h3>
                 <ul>
-                <li>Calories {(props.calories)?.toFixed(2)+' kcal'}</li>
-                    {props.digest?.map((elem, i) =>
-                    <li key={i}>
-                        <span>{elem.label} </span>
-                        <span>{(elem.total).toFixed(2)} </span>
-                        <span>{elem.unit} </span>
-                    </li>
-                )}
+                <li><span className={styles.nutrient__header}>Calories</span> <span className={styles.nutrient__total}>{(props.calories)?.toFixed(2)}</span> kcal</li>
+                    {props.digest?.map((elem, i) =>{
+                    if(i > 3) {
+                        return (
+                    <li className={styles.nutrient__item} key={i}>
+                        <span className={styles.nutrient__header}>{elem.label} </span>
+                        <span className={styles.nutrient__total}>{(elem.total).toFixed(2)}</span>
+                        <span> {elem.unit} </span>
+                    </li>)
+                }
+                })}
             </ul>
+        </div>
+        </div>
         </div>
         </div>
         </div>
