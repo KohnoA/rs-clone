f9a24280c273b6f5a9d0ba4fb61ab4d8ae441dad
KohnoA <san.kohno@yandex.ru>
2023-02-26 01:35:13 +0300

    feat: create custom arrow buttons for carusel

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-import RecipeCard from '../../components/RecipeCard/RecipeCard';
+import { Navigation } from 'swiper';
+// import RecipeCard from '../../components/RecipeCard/RecipeCard';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-import 'swiper/scss/scrollbar';
 import styles from './Main.module.scss';
 
 const Main: React.FC = () => {
@@ -13,47 +11,34 @@ const Main: React.FC = () => {
     <div className='container page'>
       <h3 className={ styles.title }>Dishes of the day</h3>
 
-      <Swiper
-        modules={[ Navigation, Pagination, Scrollbar, A11y ]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id atque necessitatibus eligendi maiores ipsa sunt dolorem. Deserunt reprehenderit asperiores provident ratione, eos mollitia, perferendis error numquam quas officia expedita minima odit architecto quo sunt magni dolorum et autem quod dignissimos!</p>
-            <button>Button</button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
-            <button>Button</button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
-            <button>Button</button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
-            <button>Button</button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
-            <button>Button</button>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className={ styles.carusel_wrapper }>
+        <Swiper
+          className={ styles.carusel }
+          modules={[ Navigation ]}
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation={{
+            nextEl: '.button-next-slide',
+            prevEl: '.button-prev-slide',
+          }}
+          loop
+        >
+          {
+            new Array(10).fill('Button').map((item, index) => {
+              return <SwiperSlide key={index}>
+                <div>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id atque necessitatibus eligendi maiores ipsa sunt dolorem. Deserunt reprehenderit asperiores provident ratione, eos mollitia, perferendis error numquam quas officia expedita minima odit architecto quo sunt magni dolorum et autem quod dignissimos!</p>
+                  <button>{ item }</button>
+                </div>
+              </SwiperSlide>
+            })
+          }
+          
+        </Swiper>
+
+        <div className={`button-prev-slide ${styles.prev_slide}`}></div>
+        <div className={`button-next-slide ${styles.next_slide}`}></div>
+      </div>
     </div>
   )
 }
