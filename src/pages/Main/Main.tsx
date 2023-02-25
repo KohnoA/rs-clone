15c716093a1733c7962bdbd216f1c966669c6a52
KohnoA <san.kohno@yandex.ru>
2023-02-25 23:40:17 +0300

    feat: connected swiper js

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,41 +1,59 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
-// import RecipeList from '../../components/RecipeCard/RecipeList/RecipeList';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
+import styles from './Main.module.scss';
 
 const Main: React.FC = () => {
-  // https://www.npmjs.com/package/react-multi-carousel
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1
-    }
-  };
 
   return (
-    <div className={'container page'} style={{maxWidth: '1000px', margin: '0 auto'}}>
-      <Carousel responsive={responsive}>
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-      </Carousel>
+    <div className='container page'>
+      <h3 className={ styles.title }>Dishes of the day</h3>
+
+      <Swiper
+        modules={[ Navigation, Pagination, Scrollbar, A11y ]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id atque necessitatibus eligendi maiores ipsa sunt dolorem. Deserunt reprehenderit asperiores provident ratione, eos mollitia, perferendis error numquam quas officia expedita minima odit architecto quo sunt magni dolorum et autem quod dignissimos!</p>
+            <button>Button</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
+            <button>Button</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
+            <button>Button</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
+            <button>Button</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, obcaecati.</p>
+            <button>Button</button>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
