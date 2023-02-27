d99a77adf06b43ff3cd8c2b1b0fb873149a7f8a7
Nexuslolz <a.klyosov@inbox.ru>
2023-02-28 01:53:06 +0500

    feat: add const object

---
+++ src/components/Cabinet/components/ProfileData.ts
@@ -0,0 +1,15 @@
+export interface IProfileData {
+    age: string;
+    city: string;
+    height: string;
+    weigth: string;
+    about: string;
+}
+
+export const ProfileData: IProfileData = {
+    age: '...',
+    city: '...',
+    height: '...',
+    weigth: '...',
+    about: 'About you...',
+}
