b0fea40ac82fd7b1cde1d111e4a5a7ff043de01c
Nexuslolz <a.klyosov@inbox.ru>
2023-02-28 03:38:07 +0500

    feat: update favorite page render

--- src/components/Favourite/FavoriteList/FavoriteList.tsx
+++ src/components/Favourite/FavoriteList/FavoriteList.tsx
@@ -1,27 +1,40 @@
-import { IRecipesData } from '../../../types/types';
-import RecipeCard from '../../RecipeCard/RecipeCard';
+import RecipeCard from '../../RecipeCard/RecipeCard'
 import iconType from '../../../assets/icons/food.svg'
 import kcalIcon from '../../../assets/icons/kcal.svg'
+import { useFetchFavoriteRecipesQuery } from '../../../sevices/foodService.api'
+import { useEffect } from 'react'
 
+interface IProps {
+  id: string
+  setError: (error: boolean) => void
+}
 
-const FavoriteList: React.FC<IRecipesData> = ({recipe, id}: IRecipesData) => {
+const FavoriteList: React.FC<IProps> = ({ id, setError }: IProps) => {
+  const { data, isError } = useFetchFavoriteRecipesQuery(id)
 
-    return (
-      <>
-            <RecipeCard
-                route='favorite'
-                key={1}
-                id={id}
-                header={recipe.cuisineType}
-                image={recipe.image}
-                type={recipe.dishType}
-                typeIcon={iconType}
-                kcalIcon={kcalIcon}
-                kcal={Math.round(Number(recipe.calories))}
-                title={recipe.label}
-                  />
-        </>
-    );
-};
+  useEffect(() => {
+    if (isError) {
+      setError(true)
+    }
+  }, [isError])
 
- export default FavoriteList;
+  if (!data) {
+    return null
+  }
+
+  return (
+    <RecipeCard
+      route='favorite'
+      id={id}
+      header={data.recipe.cuisineType}
+      image={data.recipe.image}
+      type={data.recipe.dishType}
+      typeIcon={iconType}
+      kcalIcon={kcalIcon}
+      kcal={Math.round(Number(data.recipe.calories))}
+      title={data.recipe.label}
+    />
+  )
+}
+
+export default FavoriteList
