84448dddf2d81ec7fc5b00a238d30dbc380d4532
Nexuslolz <a.klyosov@inbox.ru>
2023-02-27 12:24:58 +0500

    feat: update getting data to fav page

--- src/pages/Favorite/FavoritePage.tsx
+++ src/pages/Favorite/FavoritePage.tsx
@@ -1,33 +1,59 @@
-import { useSelector } from 'react-redux';
-import FavoriteList from '../../components/Favourite/FavoriteList/FavoriteList';
-import { getFavoriteList } from '../../store/selectors/favoriteSelectors';
-import styles from './FavoritePage.module.scss';
-import * as API from '../../constants/foodApi'
-import {useMemo} from 'react'
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { useSelector } from 'react-redux'
+import FavoriteList from '../../components/Favourite/FavoriteList/FavoriteList'
+import { getFavoriteList } from '../../store/selectors/favoriteSelectors'
+import styles from './FavoritePage.module.scss'
+import Loader from '../../components/Loader/Loader'
+import { useFetchFavoriteRecipesQuery } from '../../sevices/foodService.api'
 
-const FavoritePage = () => {
-    const favArr = useSelector(getFavoriteList)
+const FavoritePage: React.FC = () => {
+  const favArr = useSelector(getFavoriteList)
 
-    const url = useMemo(() => {
-        const params = new URLSearchParams('')
-        params.set('type', API.TYPE)
-        params.set('app_id', API.ID_RECIPES)
-        params.set('app_key', API.API_KEY_RECIPES)
+  const arrParams = favArr.map((id) => {
+    const { data, isLoading, error } = useFetchFavoriteRecipesQuery(id)
+    return { data, isLoading, error }
+  })
 
-        return (id: string) => `${API.DOMAIN}/${API.RECIPES}/${id}?${String(params)}`
-    }, [] )
+  arrParams.forEach((item, i) => {
+    if (item.data == undefined) {
+      arrParams.splice(i, 1)
+    }
+  })
 
-    return (
-        <div className={styles.favoritePage}>
-            <h1>Favorite recipes</h1>
-        <div className={styles.favoritePage__wrapper}>
-            {favArr.length > 0
-            ? favArr.map((id: string) =>  <FavoriteList key={id} url={url(id)}/> )
-            : <h1>There is no favorite recipes yet.</h1>}
-        </div>
-        </div>
-    );
+  console.log(arrParams)
 
-};
+  return (
+    <div className={styles.favoritePage}>
+      <h1>Favorite recipes</h1>
 
-export default FavoritePage;
+      <div
+        className={
+          favArr.length === 0 ||
+          arrParams[0].error ||
+          arrParams[0].isLoading ||
+          arrParams.map((item) => item.data === undefined)
+            ? styles.favoritePage__wrapper_empty
+            : styles.favoritePage__wrapper
+        }
+      >
+        {arrParams[0].error ? (
+          <div>
+            <h1>
+              Error has occured. Network Error. Maybe it`s too many requests. Please, try later.
+            </h1>
+          </div>
+        ) : arrParams[0].isLoading ? (
+          <Loader />
+        ) : favArr.length === 0 ? (
+          <h1>There is no favorite recipes yet.</h1>
+        ) : (
+          arrParams.map((item, i) => (
+            <FavoriteList key={item.data!.recipe.uri} recipe={item.data!.recipe} id={favArr[i]} />
+          ))
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default FavoritePage
